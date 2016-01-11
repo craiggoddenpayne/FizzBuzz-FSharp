78721a92f08d4bf0b973c2e4e635e3290365c005
Craig Godden-Payne <craig@goddenpayne.co.uk>
2016-01-11 21:00:05 +0000

    Refactored compute to work for modulus 5 and 3

--- FizzBuzz/FizzBuzz.fs
+++ FizzBuzz/FizzBuzz.fs
@@ -1,6 +1,7 @@
 ﻿module FizzBuzz
 
 let Compute value =
-    if value % 3 = 0 then "Fizz"
+    if (value % 3 = 0) && (value % 5 = 0) then "FizzBuzz"
+    else if value % 3 = 0 then "Fizz"
     else if value % 5 = 0 then "Buzz"
     else value.ToString()
