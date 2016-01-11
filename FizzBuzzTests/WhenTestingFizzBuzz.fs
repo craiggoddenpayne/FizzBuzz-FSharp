79f2483b7e59c91119488a41eff9b80dc0f389a3
Craig Godden-Payne <craig@goddenpayne.co.uk>
2016-01-11 20:49:09 +0000

    Added next test scenario
    When passing 3 it should return Fizz

--- FizzBuzzTests/WhenTestingFizzBuzz.fs
+++ FizzBuzzTests/WhenTestingFizzBuzz.fs
@@ -7,3 +7,6 @@ let WhenCallingComputeWith1ItShouldReturn1() = Assert.That(FizzBuzz.Compute 1, I
 
 [<Test>]
 let WhenCallingComputeWith2ItShouldReturn2() = Assert.That(FizzBuzz.Compute 2, Is.EqualTo 2)
+
+[<Test>]
+let WhenCallingComputeWith3ItShouldReturnFizz() = Assert.That(FizzBuzz.Compute 3, Is.EqualTo "Fizz")
