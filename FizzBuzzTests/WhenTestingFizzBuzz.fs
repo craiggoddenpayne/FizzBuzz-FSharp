4ba8bbb5948b78c0c287e8a44eb96b0763bb92d8
Craig Godden-Payne <craig@goddenpayne.co.uk>
2016-01-11 20:47:38 +0000

    Added test for next scenario
    When passing 2 now fails

--- FizzBuzzTests/WhenTestingFizzBuzz.fs
+++ FizzBuzzTests/WhenTestingFizzBuzz.fs
@@ -4,3 +4,6 @@ open NUnit.Framework
 
 [<Test>]
 let WhenCallingComputeWith1ItShouldReturn1() = Assert.That(FizzBuzz.Compute 1, Is.EqualTo 1)
+
+[<Test>]
+let WhenCallingComputeWith2ItShouldReturn2() = Assert.That(FizzBuzz.Compute 2, Is.EqualTo 2)
