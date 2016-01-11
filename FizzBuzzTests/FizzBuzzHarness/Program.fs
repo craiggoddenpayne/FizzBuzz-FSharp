89fa84830a146d45986ad77ea1c1de4645c0040f
Craig Godden-Payne <craig@goddenpayne.co.uk>
2016-01-11 21:06:33 +0000

    Added program to print out values from 1 to 100

--- FizzBuzzTests/FizzBuzzHarness/Program.fs
+++ FizzBuzzTests/FizzBuzzHarness/Program.fs
@@ -1,3 +1,8 @@
-﻿[<EntryPoint>]
-let main args = 
+﻿open FizzBuzz
+
+[<EntryPoint>]
+let main args =
+    for i in [1..100] do
+        System.Console.WriteLine(FizzBuzz.Compute(i))
+    let a = System.Console.ReadKey()
     0 
